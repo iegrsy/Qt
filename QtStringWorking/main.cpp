114bd3789997c0755e91993c008710b2119511f4
ibrahim ethem gürsoy <iegursoy28@gmail.com>
2017-06-29 13:56:48 +0300

    Add files via upload

--- QtStringWorking/main.cpp
+++ QtStringWorking/main.cpp
@@ -13,45 +13,16 @@
 using namespace std;
 
 void tabloVerileriniSirala(int sutun);
-
+void qstringDenemeleri();
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QString testCumle = "Çiftçi ile Padişah Hikayesi. "
-                        "Bir varmış, bir yokmuş. Evvel zaman içinde bir gün padişahlar "
-                        "padişahı av için şehirden uzaklaşmış. "
-                        "Yolda giderken pek çok insanın çalıştığı bir tarla görmüş. "
-                        "%1 %2 %3 %4 %5 %6";
+    tabloVerileriniSirala(2);
+    qstringDenemeleri();
 
-    qDebug() << testCumle <<endl;
-    qDebug() << testCumle.arg("1.deneme","2.deneme","3.deneme","4.deneme","5.deneme","6.deneme")<<endl;
-    //(condition) ? (if_true) : (if_false)
-    qDebug() << ((testCumle.contains("ile",Qt::CaseInsensitive)) ? ("içerir."):("içermez."))<<endl;
-    qDebug() << testCumle.at(1) <<endl;
-    qDebug() << testCumle.begin() <<endl;
-    qDebug() << testCumle.replace("ile","ve") <<endl;
-    qDebug() << testCumle.insert(28,"..(Burada bir ara verelim.)..") <<endl;
-    qDebug() << ((testCumle.isRightToLeft()) ? ("RightToLeft true."):("RightToLeft false.")) <<endl;
-
-    qDebug() << testCumle.length() <<endl;
-    qDebug() << testCumle.capacity() <<endl;
-    qDebug() << QString("Cümle %1 karakterden oluşur.").arg(testCumle.count()) <<endl;
-
-    qDebug() << testCumle.mid(0,20) <<endl;
-    qDebug() << testCumle.prepend("cv ektedir.") <<endl;
-    testCumle.push_back("...push_back...");
-    testCumle.push_front("...push_front...");
-    qDebug() << testCumle <<endl;
-
-    qDebug() << testCumle.remove(0,15) <<endl;
-    qDebug() << QString("hey corç").repeated(3) <<endl;
-
-    qDebug()<<testCumle.replace("a","i")<<endl;
-
-    //    system("pause");
-    //    return a.exec();
+    return a.exec();
 }
 
 void tabloVerileriniSirala(int sutun)
@@ -93,3 +64,36 @@ void tabloVerileriniSirala(int sutun)
     }
 }
 
+void qstringDenemeleri()
+{
+    QString testCumle = "Çiftçi ile Padişah Hikayesi. "
+                        "Bir varmış, bir yokmuş. Evvel zaman içinde bir gün padişahlar "
+                        "padişahı av için şehirden uzaklaşmış. "
+                        "Yolda giderken pek çok insanın çalıştığı bir tarla görmüş. "
+                        "%1 %2 %3 %4 %5 %6";
+
+    qDebug() << testCumle <<endl;
+    qDebug() << testCumle.arg("1.deneme","2.deneme","3.deneme","4.deneme","5.deneme","6.deneme")<<endl;
+    //(condition) ? (if_true) : (if_false)
+    qDebug() << ((testCumle.contains("ile",Qt::CaseInsensitive)) ? ("içerir."):("içermez."))<<endl;
+    qDebug() << testCumle.at(1) <<endl;
+    qDebug() << testCumle.begin() <<endl;
+    qDebug() << testCumle.replace("ile","ve") <<endl;
+    qDebug() << testCumle.insert(28,"..(Burada bir ara verelim.)..") <<endl;
+    qDebug() << ((testCumle.isRightToLeft()) ? ("RightToLeft true."):("RightToLeft false.")) <<endl;
+
+    qDebug() << testCumle.length() <<endl;
+    qDebug() << testCumle.capacity() <<endl;
+    qDebug() << QString("Cümle %1 karakterden oluşur.").arg(testCumle.count()) <<endl;
+
+    qDebug() << testCumle.mid(0,20) <<endl;
+    qDebug() << testCumle.prepend("cv ektedir.") <<endl;
+    testCumle.push_back("...push_back...");
+    testCumle.push_front("...push_front...");
+    qDebug() << testCumle <<endl;
+
+    qDebug() << testCumle.remove(0,15) <<endl;
+    qDebug() << QString("hey corç").repeated(3) <<endl;
+
+    qDebug()<<testCumle.replace("a","i")<<endl;
+}
